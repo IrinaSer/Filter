b0fc36593aaf8d854993c3dc22a2931747f722af
IrinSer <serebryakova.irin.i@gmail.com>
2020-05-27 02:02:00 +0300

    add pug

--- src/components/FilterRow.vue
+++ src/components/FilterRow.vue
@@ -1,83 +1,75 @@
-<template>
-  <div
-    class="grid-x"
-    :id="row.id">
-    <select
+<template lang="pug">
+  .grid-x(:id="row.id")
+    select(
       name="field-type"
       class="medium-4 cell"
       v-model="row.selectType"
-      @change="changeValue">
-      <option
+      @change="changeValue")
+      option(
         v-for="(field,i) in content.fieldsType"
         :value="field.value"
         :key="i"
-      >{{ field.name }}
-      </option>
-    </select>
-    <select
+      ) {{ field.name }}
+    select(
       name="text"
       class="medium-4 cell"
       v-model="row.selectOperation"
-      v-show="row.selectType === 'text'">
-      <option
+      v-show="row.selectType === 'text'")
+      option(
         v-for="(field,i) in content.textSelect"
         :value="field"
         :key="i"
-      >{{ field }}
-      </option>
-    </select>
-    <select
+      ) {{ field }}
+    select(
       name="number"
       class="medium-4 cell"
       v-model="row.selectOperation"
-      v-show="row.selectType === 'number'">
-      <option
+      v-show="row.selectType === 'number'")
+      option(
         v-for="(field,i) in content.numberSelect"
         :value="field"
         :key="i"
-      >{{ field }}
-      </option>
-    </select>
-    <input
+      ) {{ field }}
+    input(
       name="text-value"
       type="text"
       data-val="text"
       class="medium-4 cell"
       v-model="row.inputValue"
-      v-show="row.selectType === 'text'">
-    <input
+      v-show="row.selectType === 'text'")
+    input(
       name="number-value"
       type="number"
       data-val="num"
       class="medium-4 cell"
       v-model="row.inputValue"
-      v-show="row.selectType === 'number'">
-    <span
-      class="close"
+      v-show="row.selectType === 'number'")
+    span.close(
       v-show="count>1"
-      @click="deleteFilterRow"></span>
-  </div>
+      @click="deleteFilterRow")
 </template>
 
 <script>
-  import {mapGetters} from 'vuex';
+import { mapGetters } from "vuex";
 
-  export default {
-    props: ['content', 'count', 'row'],
-    computed: {
-      ...mapGetters({
-        textSelect: 'textValue',
-        numberSelect: 'numberValue'
-      })
+export default {
+  props: ["content", "count", "row"],
+  computed: {
+    ...mapGetters({
+      textSelect: "textValue",
+      numberSelect: "numberValue",
+    }),
+  },
+  methods: {
+    changeValue: function() {
+      this.row.selectType == "number"
+        ? (this.row.selectOperation = this.numberSelect)
+        : (this.row.selectOperation = this.textSelect);
+      this.row.inputValue = "";
     },
-    methods: {
-      changeValue: function () {
-        (this.row.selectType == 'number') ? this.row.selectOperation = this.numberSelect : this.row.selectOperation = this.textSelect
-        this.row.inputValue = '';
-      },
-      deleteFilterRow: function () {
-        this.$store.commit('deleteFilterRow', this.row.id)
-      }
-    }
-  }
+    deleteFilterRow: function() {
+      this.$store.commit("deleteFilterRow", this.row.id);
+    },
+  },
+};
 </script>
--- src/components/MainFilter.vue
+++ src/components/MainFilter.vue
@@ -1,86 +1,76 @@
-<template>
-  <div class="grid-container">
-    <div class="main-app">
-      <app-row
+<template lang="pug">
+  .grid-container
+    .main-app
+      app-row(
         v-for="row in filterRows"
         :key="row.id"
         :content="setup"
         :row="row"
-        :count="filterRows.length"
-      ></app-row>
-      <a
+        :count="filterRows.length")
+      a(
         href="#"
         id="add"
         v-show="filterRows.length<10"
         @click="createFilterRow"
-        class="add-condition">Add condition</a>
+        class="add-condition"
+        ) Add condition
       <hr>
-      <button
-        class="button"
+      button.button(
         @click="pushToStore"
-      >Apply
-      </button>
-      <button
-        class="button secondary"
+      ) Apply
+      button.button.secondary(
         @click="clearFilter"
-      >Clear filter
-      </button>
-      <h4>Result: </h4>
-      <pre>
-        <code>
-          {{ result }}
-         </code>
-      </pre>
-
-    </div>
-  </div>
+      ) Clear filter
+      h4 Result:
+      pre
+        code {{ result }}
 </template>
 
 <script>
-  import Row from '../components/FilterRow.vue'
-  import {mapGetters} from 'vuex';
+import Row from "../components/FilterRow.vue";
+import { mapGetters } from "vuex";
 
-  export default {
-    components: {
-      appRow: Row
+export default {
+  components: {
+    appRow: Row
+  },
+  computed: {
+    ...mapGetters({
+      filterRows: "rows",
+      setup: "setup",
+      result: "result"
+    })
+  },
+  methods: {
+    createFilterRow() {
+      this.$store.commit("createFilterRow");
+      this.$store.commit("clearResult");
     },
-    computed: {
-      ...mapGetters({
-        filterRows: 'rows',
-        setup: 'setup',
-        result: 'result'
-      }),
-    },
-    methods: {
-      createFilterRow() {
-        this.$store.commit('createFilterRow')
-        this.$store.commit('clearResult')
-      },
-      pushToStore: function () {
-        this.$store.commit('clearResult')
+    pushToStore: function() {
+      this.$store.commit("clearResult");
 
-        for (let i in this.filterRows) {
-          let filterItem = this.filterRows[i];
-          let filterItemResult = {
-            type: '',
-            operation: '',
-            value: ''
-          };
+      for (let i in this.filterRows) {
+        let filterItem = this.filterRows[i];
+        let filterItemResult = {
+          type: "",
+          operation: "",
+          value: ""
+        };
 
-          filterItemResult.operation = filterItem.selectOperation;
-          filterItemResult.value = filterItem.inputValue;
-          filterItemResult.type = filterItem.selectType;
+        filterItemResult.operation = filterItem.selectOperation;
+        filterItemResult.value = filterItem.inputValue;
+        filterItemResult.type = filterItem.selectType;
 
-          this.$store.commit('updateResult', filterItemResult)
-        }
-      },
-      clearFilter: function () {
-        this.$store.commit('clearFilter')
-        this.createFilterRow()
+        this.$store.commit("updateResult", filterItemResult);
       }
     },
-    created: function () {
-      this.createFilterRow()
+    clearFilter: function() {
+      this.$store.commit("clearFilter");
+      this.createFilterRow();
     }
+  },
+  created: function() {
+    this.createFilterRow();
   }
+};
 </script>
